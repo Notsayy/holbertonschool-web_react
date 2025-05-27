feccf692ae4b24f4aa4ad27a14d3e828f31dec79
Notsayy <jtertre25@gmail.com>
2025-05-27 14:30:01 +0200

    Task 4

--- TypeScript/task_1/js/main.ts
+++ TypeScript/task_1/js/main.ts
@@ -35,4 +35,31 @@ interface printTeacherFunction {
 const printTeacher: printTeacherFunction = function (firstName, lastName) {
   return firstName.charAt(0) + '. ' + lastName;
 };
-console.log(printTeacher("John", "Doe")); // J. Doe
+console.log(printTeacher('John', 'Doe')); // J. Doe
+
+class StudentClass implements StudentClassInterface {
+  firstName: string;
+  lastName: string;
+  constructor(firstName: string, lastName: string) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+  }
+  workOnHomework() {
+    return 'Currently working';
+  }
+  displayName() {
+    return this.firstName;
+  }
+}
+interface StudentClassConstructor {
+  new(firstName:string, lastName:string): StudentClassInterface
+}
+interface StudentClassInterface {
+  firstName: string;
+  lastName: string;
+  workOnHomework(): string;
+  displayName(): string;
+}
+const student = new StudentClass('Alice', 'Dupont');
+console.log(student.displayName());
+console.log(student.workOnHomework());
